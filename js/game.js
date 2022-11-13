9b3993107a236debc096499af1e77625991f6a63
David Maxwell <maxxdaddy@aol.com>
2022-11-12 21:23:50 -0800

    update

--- js/game.js
+++ js/game.js
@@ -4,44 +4,63 @@ var id = function(id) { return document.getElementById(id); };
 var word;
 var yellow = "#B59F39";
 var green = "#538D4E";
+var correctLetters = 0;
+var score = 0;
+var highScore = 0;
+var row =1;
+var guessedLetters = [];
 
 function StartGame(){
     word = WORDS[Math.floor(Math.random()*WORDS.length)];
     console.log(word);
     id("1").focus();
-
+    correctLetters=0;
+    updateScore();
     id("keyboard").addEventListener("click", (e) => {
-        console.log(target);
+        const target = e.target
         if (!target.classList.contains("keyboard-button")) {
             return;
         }
         let key = target.textContent;
-        guessLetter(key);
+        id(guessNumber).value = key;
+        guessNumber++;
+        row=30 % guessNumber;
+        id(guessNumber).focus();
+        updateScore();
     });
     
     $(document).ready(function(){
         $('input').on('focusout',function(){ 
             var letter = $(this).val().toLowerCase();
             let index = word.lastIndexOf(letter)+1;
-            
+             
             if(word.includes(letter)){
-                console.log(index==this.id);
                 if(index==this.id)
                 {
                     $(this).css('background',green);
+                    correctLetters++;
+                    guessedLetters.push(letter);
+                    if(!guessedLetters.includes(letter))
+                    score += (5-row) * 50;
+                    if(correctLetters==5)
+                    {
+                        window.alert("YOU WON!!");
+                    }
                 }
                 else
                 {
                     $(this).css('background',yellow);
+                    if(!guessedLetters.includes(letter))
+                        score += (5-row) * 10;
                 }
             }
-            });
+            updateScore();
+        });
   });
 }
-
-function guessLetter(key){
-$(25+guessNumber).value = key;
-if(guessNumber<5)
-    guessNumber++;
+function updateScore(){
+    id("score").textContent = "SCORE : "+score;
+    id("highscore").textContent = "HIGH SCORE : "+highScore;
 }
+
 window.onload = StartGame;
